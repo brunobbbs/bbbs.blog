fb3875253b25f0f1a5890323350ee6d2fdcd8f89
Bruno Barbosa <bsbruno1@gmail.com>
2016-04-18 16:20:23 -0300

    update example

--- src/bbbs/blog/browser/views.py
+++ src/bbbs/blog/browser/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-
+from datetime import datetime
 from Products.Five.browser import BrowserView
 
 
 class MyFirstView(BrowserView):
     """My first BrowserView"""
 
+    def date(self):
+        return datetime.now().strftime('%d/%m/%Y')
